afe63b3229caf61c1a1f8e1ba4e4616b9b2c1726
Vilem Liepelt <17603372+buggymcbugfix@users.noreply.github.com>
2025-10-31 16:50:28 +0100

    nix: make it work on mac

--- default.nix
+++ default.nix
@@ -1,4 +1,15 @@
-{stdenv, curl, gcc, urweb, icu}:
+{
+  stdenv,
+  curl,
+  gcc,
+  urweb,
+  icu,
+}: let
+  libExt =
+    if stdenv.isDarwin
+    then "dylib"
+    else "so";
+in
   stdenv.mkDerivation rec {
     version = "0.0.1";
     name = "urweb-curl-${version}";
@@ -7,12 +18,12 @@
 
     buildInputs = [gcc];
     # TODO: Improve like urweb.nix c, cflags etc
-    configurePhase = "
-      sed -e s~@LIBCURL@~${curl.out}/lib/libcurl.so~g -e s~@LIBCURLDEV@~${curl.dev}/include~g lib.urp.in > lib.urp
-    ";
+    configurePhase = ''
+      sed -e s~@LIBCURL@~${curl.out}/lib/libcurl.${libExt}~g -e s~@LIBCURLDEV@~${curl.dev}/include~g lib.urp.in > lib.urp
+    '';
     buildPhase = ''
       ${gcc}/bin/gcc -c -I${urweb}/include/urweb -I${curl.dev}/include -I${icu.dev}/include -Isrc/c -o src/c/curl.o -Wimplicit -Wall -Werror -Wno-deprecated-declarations src/c/curl.c
-  '';
+    '';
     installPhase = "
     mkdir $out
     cp -r . $out
