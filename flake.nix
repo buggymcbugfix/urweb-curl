57b4ce8d6e2125b3f99044a1940dd31600c22155
Vilem Liepelt <17603372+buggymcbugfix@users.noreply.github.com>
2026-08-11 14:32:52 +0200

    Update inputs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = {self, nixpkgs, flake-utils} :
